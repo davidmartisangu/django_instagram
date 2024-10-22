8bf2f0aa366b5aaa12dd845254031a9e40254e8e
David Marti <davidmartisangu@DavidPC>
2024-10-22 06:03:24 +0200

    Adding messages with bootstrap style after register

--- instagram/instagram/settings.py
+++ instagram/instagram/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'django_extensions',
+    'crispy_forms',
+    'crispy_bootstrap5',
 
     'profiles',
     'posts',
@@ -130,3 +132,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
--- instagram/instagram/views.py
+++ instagram/instagram/views.py
@@ -1,19 +1,37 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth.models import User
+from django.contrib import messages
+
+from .forms import RegistrationForm
 
 # Create your views here.
 
 class HomeView(TemplateView):
     template_name="general/home.html"
 
+
 class LoginView(TemplateView):
     template_name="general/login.html"
 
-class RegisterView(TemplateView):
+
+class RegisterView(CreateView):
     template_name="general/register.html"
+    model = User
+    success_url = reverse_lazy("login")
+    form_class = RegistrationForm
+    
+    def form_valid(self, form):
+        messages.add_message(self.request, messages.SUCCESS, "Usuario creado correctamente.")
+        return super(RegisterView, self).form_valid(form)
+    
+
 
 class LegalView(TemplateView):
     template_name="general/legal.html"
+
 
 class ContactView(TemplateView):
     template_name="general/contact.html"
